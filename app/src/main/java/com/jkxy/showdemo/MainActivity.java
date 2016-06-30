4d8e8e0843b791e6e831664c6558d159af05907e
Old-Driver-ZH <OldDriver_ZH@163.com>
2016-06-30 12:30:12 +0800

    Initial commit

---
+++ app/src/main/java/com/jkxy/showdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jkxy.showdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        JSONObject object = new JSONObject();
+
+    }
+}
